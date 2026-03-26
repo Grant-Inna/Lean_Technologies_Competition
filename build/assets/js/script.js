c9348695e41593442580665aca72e207b1988c96
Inna Grant <i.grant@ficto.ru>
2026-03-27 00:17:08 +0300

    версия 0.9

--- build/assets/js/script.js
+++ build/assets/js/script.js
@@ -1,1 +1,54 @@
-$(document).ready(function(){if(0<$(".hidden_text").length){let n=$(".hidden_text");n.on("click",e=>{l($(e.target).closest(n))})}if(0<$(".qa__block").length){let n=$(".qa__block"),e=$(".qa__block_headline");e.on("click",e=>{l($(e.target).closest(n))})}function l(e){var n=$(e).find(".hidden");$(e).hasClass("open")?($(e).find(n).slideUp(300),$(e).removeClass("open")):($(e).find(n).slideDown(200),$(e).addClass("open"))}});
+$(document).ready(function () {
+   
+   /* читать дальше */
+   
+   if ($('.hidden_text').length > 0) {
+      let $parent = $('.hidden_text');
+      
+      $parent.on('click', (event) => {openAnswer($(event.target).closest($parent))});
+   }
+   
+   if ($('.qa__block').length > 0) {
+      let $parent = $('.qa__block'),
+          $btn = $('.qa__block_headline');
+      
+      $btn.on('click', (event) => {openAnswer($(event.target).closest($parent))});
+   }
+   
+   function openAnswer(target) {
+      let hidden = $(target).find('.hidden');
+      
+      if ($(target).hasClass('open')) {
+         $(target).find(hidden).slideUp(300); // ради чего всё затевалось - показать скрытое
+         $(target).removeClass('open');
+      } else {
+         $(target).find(hidden).slideDown(200);
+         $(target).addClass('open');
+      }
+   }
+   
+    /*  плоавный скролл в рамках страницы  */
+   
+   if ($('#totop').length > 0) {
+      
+      let scroll = $('body').prop('scrollHeight'),
+          height = $(window).height(),
+          $totop = $('#totop');
+      
+      if ( scroll > height - 100 ) {
+         $totop.show();
+         $totop.on( 'click', (event) => goTo(event))
+      } else {
+         $totop.hide();
+      }
+   }
+   function goTo(event) {
+      event.preventDefault();
+      let id = $('.cover_block'),
+            speed = 150;
+      
+      $('body, html').animate({ scrollTop: $(id).offset().top }, speed);
+   }
+   
+   
+});
--- develop/assets/js/script.js
+++ develop/assets/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
+   
    /* читать дальше */
-
    
    if ($('.hidden_text').length > 0) {
       let $parent = $('.hidden_text');
@@ -15,17 +15,40 @@ $(document).ready(function () {
       $btn.on('click', (event) => {openAnswer($(event.target).closest($parent))});
    }
    
-      function openAnswer(target) {
-         let hidden = $(target).find('.hidden');
-         
-         if ($(target).hasClass('open')) {
-            $(target).find(hidden).slideUp(300); // ради чего всё затевалось - показать скрытое
-            $(target).removeClass('open');
-         } else {
-            $(target).find(hidden).slideDown(200);
-            $(target).addClass('open');
-         }
+   function openAnswer(target) {
+      let hidden = $(target).find('.hidden');
+      
+      if ($(target).hasClass('open')) {
+         $(target).find(hidden).slideUp(300); // ради чего всё затевалось - показать скрытое
+         $(target).removeClass('open');
+      } else {
+         $(target).find(hidden).slideDown(200);
+         $(target).addClass('open');
       }
+   }
+   
+    /*  плоавный скролл в рамках страницы  */
+   
+   if ($('#totop').length > 0) {
+      
+      let scroll = $('body').prop('scrollHeight'),
+          height = $(window).height(),
+          $totop = $('#totop');
+      
+      if ( scroll > height - 100 ) {
+         $totop.show();
+         $totop.on( 'click', (event) => goTo(event))
+      } else {
+         $totop.hide();
+      }
+   }
+   function goTo(event) {
+      event.preventDefault();
+      let id = $('.cover_block'),
+            speed = 150;
+      
+      $('body, html').animate({ scrollTop: $(id).offset().top }, speed);
+   }
    
    
 });
